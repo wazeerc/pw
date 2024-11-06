aeea68ede6b24dd327fd538e8a390330eeb01fe7
Wazeer <58399263+wazeerc@users.noreply.github.com>
2024-11-06 17:24:15 +0400

    feat: Add basic logic for non-interactive password generation command

---
+++ cmd/non_interactive.go
@@ -0,0 +1,37 @@
+package cmd
+
+import (
+	password_generator "pw/utils"
+
+	"github.com/spf13/cobra"
+)
+
+// This is the default command that runs in non-interactive mode, i.e. it uses flags.
+var nonInteractiveMode = &cobra.Command{
+	Use:   "gen",
+	Short: "Generate a password",
+	Long: `Generate a password with the specified length and character composition.
+For example, to generate a password with a length of 16 characters and 4 digits, 
+you would run the following command: pw generate -l 16 -n -s`,
+
+	Run: getPassword,
+}
+
+func init() {
+	rootCmd.AddCommand(nonInteractiveMode)
+
+	nonInteractiveMode.Flags().IntP("length", "l", 12, "Length of the password")
+	nonInteractiveMode.Flags().BoolP("numbers", "n", false, "Include digits in the password")
+	nonInteractiveMode.Flags().BoolP("symbols", "s", false, "Include symbols in the password")
+}
+
+func getPassword(cmd *cobra.Command, args []string) {
+	length, _ := cmd.Flags().GetInt("length")
+	isDigits, _ := cmd.Flags().GetBool("numbers")
+	isSymbols, _ := cmd.Flags().GetBool("symbols")
+
+	password := password_generator.GeneratePassword(length, isDigits, isSymbols)
+
+	cmd.Println("Your password has been generated successfully!")
+	cmd.Printf("Password: %s\n", password)
+}
--- cmd/root.go
+++ cmd/root.go
@@ -12,16 +12,9 @@ import (
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "pw",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+	Short: "🔐 Effortlessly Generate Robust Passwords!",
+	Long: `pw is a minimal CLI application designed to effortlessly generate robust and secure passwords.
+You can customize the length and character composition to suit your security needs.`,
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -34,13 +27,6 @@ func Execute() {
 }
 
 func init() {
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
-	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.pw.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	// TODO: Run in interactive mode
+	// rootCmd.Flags().BoolP("interactive", "i", false, "Run in interactive mode")
 }
